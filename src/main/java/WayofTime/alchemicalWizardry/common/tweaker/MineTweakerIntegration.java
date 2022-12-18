1ede50316bbea3d3446f926de65884583909ea8e
Jerome226 <55724525+Jerome226@users.noreply.github.com>
2022-12-18 02:20:13 -0500

    Fix Crash with Curseforge CraftTweaker

--- src/main/java/WayofTime/alchemicalWizardry/common/tweaker/MineTweakerIntegration.java
+++ src/main/java/WayofTime/alchemicalWizardry/common/tweaker/MineTweakerIntegration.java
@@ -1,30 +1,19 @@
 package WayofTime.alchemicalWizardry.common.tweaker;
 
 import minetweaker.MineTweakerAPI;
-import minetweaker.MineTweakerImplementationAPI;
-import minetweaker.util.IEventHandler;
 
 /**
  * MineTweaker3 Integration by joshie *
  */
-public class MineTweakerIntegration {
-    public static void register() {
+public class MineTweakerIntegration 
+{
+    public static void register() 
+    {
         MineTweakerAPI.registerClass(Alchemy.class);
         MineTweakerAPI.registerClass(Binding.class);
         MineTweakerAPI.registerClass(BloodAltar.class);
         MineTweakerAPI.registerClass(BloodOrb.class);
         MineTweakerAPI.registerClass(FallingTower.class);
         MineTweakerAPI.registerClass(HarvestMoon.class);
-
-        MineTweakerImplementationAPI.onRollbackEvent(new HandleLateAdditionsAndRemovals());
-        MineTweakerImplementationAPI.onPostReload(new HandleLateAdditionsAndRemovals());
-    }
-
-    public static class HandleLateAdditionsAndRemovals
-            implements IEventHandler<MineTweakerImplementationAPI.ReloadEvent> {
-        @Override
-        public void handle(MineTweakerImplementationAPI.ReloadEvent event) {
-            BloodOrb.applyAdditionsAndRemovals();
-        }
     }
 }
